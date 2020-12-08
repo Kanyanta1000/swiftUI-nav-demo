3828c73f45416b8eec04d3da8df8edf7c8cf1a22
Kanyanta Mubanga <emailkanyanta@gmail.com>
2020-12-08 11:31:45 +0300

    Initial Commit

---
+++ SwiftUINavDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUINavDemo
+//
+//  Created by Kanyanta Mubanga on 08.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUINavDemo/SwiftUINavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavDemoApp.swift
+//  SwiftUINavDemo
+//
+//  Created by Kanyanta Mubanga on 08.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
